c4fc6e47f85a909491c358090d3f538f3cad1322
Ryan Whitmore <ryan.whit@outlook.com>
2022-09-11 17:43:07 -0700

    Added POST Route for new notes.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // GET Route for notes content
 app.get('/notes', (req, res) => {
@@ -17,33 +17,67 @@ app.get('/notes', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
 
-function readFile(filePath) {
-  try {
-    const data = fs.readFile(filePath, { encoding: 'utf8' });
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 // GET Route for database notes
 app.get('/api/notes', (req, res) => {
+	console.info(`${req.method} request received to get all the notes.`);
 	const db = fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf-8');
-	console.log('Called the `api/notes` route.');
-	console.log('Notes contents: ', db);
 	res.json(JSON.parse(db));
 });
 
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+ const writeToFile = (destination, content) =>
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+	 err ? console.error(err) : console.info(`\nData written to ${destination}`)
+ );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
 // POST Route for database notes
 app.post('/api/notes', (req, res) => {
-    const db = fs.readFile('./db/db.json');
-    res.json(db)
+	console.info(`${req.method} request received to add a note.`);
+
+	const { title, text } = req.body;
+
+	if (req.body) {
+		const newNote = {
+			title,
+			text,
+			// tip_id: uuid(),
+		};
+
+		readAndAppend(newNote, './db/db.json');
+		// TODO: return the new note?
+		res.json(`Note added successfully`);
+	} else {
+		res.error('Error in adding tip');
+	}
 });
 
 // Wildcard route to return main index
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/index.html'))
-// );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
